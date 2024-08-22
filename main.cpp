384c4f64923f878ac8162f2fe940f0e4e279a2e3
emersonlondono <emersonlondono1112799@correo.itm.edu.co>
2024-08-22 17:15:59 -0500

    vsss xoomi

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ Write your code in this editor and press "Run" button to compile and execute it.
 int main()
 {
     std::cout<<"Hello World";
+    std::cout<< 1+1; 
     //comit comentarios
-
+    //868524
     return 0;
 }
